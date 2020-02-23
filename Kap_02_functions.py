b104c57b37b3c49c1667821c88faafe0f0dc850e
Martin Köhler <mjkoehler1@gmx.de>
2020-02-23 15:17:01 +0100

    23.2. nachmittags

--- Kap_02_functions.py
+++ Kap_02_functions.py
@@ -1,6 +1,44 @@
 # Tabelle anlegen
 # ! Achtung: alles in pandas machen
 # ToDo  zur Installation: pip install python-pptx
+"""
+slide_layouts[x]
+0 Title (presentation title slide)
+1 Title and Content
+2 Section Header (sometimes called Segue)
+3 Two Content (side by side bullet textboxes)
+4 Comparison (same but additional title for each side by side content box)
+5 Title Only
+6 Blank
+7 Content with Caption
+8 Picture with Caption
+ 
+ shapes - 6 verschiedene "Hauptarten"
+ 1 autoshape: 180 verschiedene
+ 2 picture
+ 3 grahic frame - kann Tabelle, Diagramm
+ 4 group shape - eine Menge von Shapes
+ 5 line / contector
+ 6 content part
+
+Gebräuchliste shapes:
+shape shapes – auto shapes with fill and an outline
+text boxes – auto shapes with no fill and no outline
+placeholders – auto shapes that can appear on a slide layout or master and be inherited on slides that use that layout, allowing content to be added that takes on the formatting of the placeholder
+line/connector – as described above
+picture – as described above
+table – that row and column thing
+chart – pie chart, line chart, etc.
+smart art – not supported yet, although preserved if present
+media clip – video or audio
+
+Alles Shapes werden über einen Shape-Tree verwaltet - Zugriff: 
+
+    shapes = slide.shape
+
+
+ """
+
 from pptx import Presentation
 from pptx.util import Inches
 import os
